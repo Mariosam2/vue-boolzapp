cda201a4f54dd82f136f72ba5fb9574259437ec6
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-02 11:52:46 +0100

    added data structure and working on sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import data from './config.js';
+console.log(data);
 createApp({
     data() {
         return {
