d4848ba82aa006073bbd7c1edad961d204b6bdbd
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-04 17:09:32 +0100

    edited automatic scroll

--- Milestone_5_bonus-extra/assets/js/app.js
+++ Milestone_5_bonus-extra/assets/js/app.js
@@ -1,4 +1,5 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { nextTick } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import data from './config.js';
 const DateTime = luxon.DateTime;
 // prova luxon
@@ -32,7 +33,7 @@ createApp({
         setActiveContact(index) {
             this.activeContact = index;
         },
-        sendMessage() {
+        sendMessage(event) {
             let contact = this.contacts[this.activeContact];
             let now = DateTime.now();
             if (this.newMessageText.replace(/ /g, "").length !== 0) {
@@ -58,71 +59,82 @@ createApp({
                 status: 'received',
                 hoursMinutes: now.toLocaleString(DateTime.TIME_SIMPLE),
             }
-            console.log(newMessage.message);
-            //console.log(newMessage.date);
+            //console.log(newMessage.message);
+            //console.log(newMessage.date)+
             this.contacts[this.activeContact].messages.push(newMessage);
-        },
-        getContacts() {
-            this.contacts.forEach(contact => {
-                if (!contact.name.toLowerCase().includes(this.contactName.toLowerCase())) {
-                    contact.visible = false;
-                } else {
-                    contact.visible = true;
-                }
-            });
-        },
-        onClick(index) {
-            // reset the clicked statuses
-            let message = this.contacts[this.activeContact].messages[index];
-            this.contacts[this.activeContact].messages.forEach(msg => {
-                if (msg !== message)
-                    msg.clicked = false;
-            });
-            //console.log(message);
-            // change the clicked status on the targeted message
-            message.clicked = !message.clicked;
-            //console.log(message);
-        },
-        removeMessage(index) {
-            /* this.contacts[this.activeContact].messages.message[index].clicked = false; */
-            let contact = this.contacts[this.activeContact];
-            this.contacts[this.activeContact].messages.splice(index, 1);
-            this.isEmpty(contact);
-            //console.log(this.contacts[this.activeContact].messages);
-        },
-        isEmpty(contact) {
-            let messages = this.contacts[this.activeContact].messages;
-            if (messages.length != 0) {
+            //console.log(event.target);
+            let element = this.$refs.message[this.$refs.message.length - 1];
+            console.log(element)
+            nextTick(() => {
+                element.scrollIntoView();
+            })
+
+
+
+
+
+            },
+                getContacts() {
+                this.contacts.forEach(contact => {
+                    if (!contact.name.toLowerCase().includes(this.contactName.toLowerCase())) {
+                        contact.visible = false;
+                    } else {
+                        contact.visible = true;
+                    }
+                });
+            },
+                onClick(index) {
+                // reset the clicked statuses
+                let message = this.contacts[this.activeContact].messages[index];
+                this.contacts[this.activeContact].messages.forEach(msg => {
+                    if (msg !== message)
+                        msg.clicked = false;
+                });
+                //console.log(message);
+                // change the clicked status on the targeted message
+                message.clicked = !message.clicked;
+                //console.log(message);
+            },
+                removeMessage(index) {
+                /* this.contacts[this.activeContact].messages.message[index].clicked = false; */
+                let contact = this.contacts[this.activeContact];
+                this.contacts[this.activeContact].messages.splice(index, 1);
+                this.isEmpty(contact);
+                //console.log(this.contacts[this.activeContact].messages);
+            },
+                isEmpty(contact) {
+                let messages = this.contacts[this.activeContact].messages;
+                if(messages.length != 0) {
                 contact.empty = false;
             } else {
                 contact.empty = true;
             }
 
-            //console.log(contact.empty)
-        },
-        getHoursMinutes() {
-            this.contacts.forEach(contact => {
-                contact.messages.forEach(message => {
-                    //riformatto le date aggiungendo una virgola in modo che tutte le date (anche quelle dei nuovi messaggi, vedi removeMessage e sendMessage) seguano la stessa formattazione DATETIME_SHORT_WITH_SECONDS)
-                    message.date = message.date.split(' ').join(', ')
-                    let obj = DateTime.fromFormat(message.date, 'dd/MM/yyyy, HH:mm:ss');
-                    //console.log(obj, obj.toLocaleString(DateTime.TIME_SIMPLE))
-                    //salvo solo l'orario del messaggio in una nuova proprietà hoursMinutes (che poi mostrerò dinamicamente nell'html)
-                    message.hoursMinutes = obj.toLocaleString(DateTime.TIME_SIMPLE);
-                    //console.log(message);
-                })
-            })
-        },
-        getRandomArbitrary(min, max) {
-            return Math.floor(Math.random() * (max - min) + min);
-        },
-        randomResponse(){
-            console.log(this.getRandomArbitrary(0, this.quotes.length));
-            return this.quotes[this.getRandomArbitrary(0, this.quotes.length)];
-        }
-
+        //console.log(contact.empty)
     },
-    mounted() {
-        this.getHoursMinutes()
+    getHoursMinutes() {
+        this.contacts.forEach(contact => {
+            contact.messages.forEach(message => {
+                //riformatto le date aggiungendo una virgola in modo che tutte le date (anche quelle dei nuovi messaggi, vedi removeMessage e sendMessage) seguano la stessa formattazione DATETIME_SHORT_WITH_SECONDS)
+                message.date = message.date.split(' ').join(', ')
+                let obj = DateTime.fromFormat(message.date, 'dd/MM/yyyy, HH:mm:ss');
+                //console.log(obj, obj.toLocaleString(DateTime.TIME_SIMPLE))
+                //salvo solo l'orario del messaggio in una nuova proprietà hoursMinutes (che poi mostrerò dinamicamente nell'html)
+                message.hoursMinutes = obj.toLocaleString(DateTime.TIME_SIMPLE);
+                //console.log(message);
+            })
+        })
+    },
+    getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    },
+    randomResponse() {
+        //console.log(this.getRandomArbitrary(0, this.quotes.length));
+        return this.quotes[this.getRandomArbitrary(0, this.quotes.length)];
     }
+
+},
+    mounted() {
+    this.getHoursMinutes()
+}
 }).mount('#app')
